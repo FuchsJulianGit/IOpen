bf0d54df9e1f5b4c8cee8e4bf1c1ceefef486c72
martin01034 <50406302+martin01034@users.noreply.github.com>
2019-06-09 22:08:51 +0200

    Update Index.py
    Finished the code

--- Index.py
+++ Index.py
@@ -3,80 +3,63 @@ import keras.models as models
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import ImageDraw
-#import RPi.GPIO as io
+import RPi.GPIO as io
 import time
 
-#import camera
-#
+from picamera import PiCamera
+from time import sleep
 
+io.setmode(io.BOARD) #Pinnumber instead of gpio numbers
+io.setup(31,io.OUT)  #Pin 31 als Ausgang
+io.output(31,io.LOW) #Pin 31 wieder aus
+io.cleanup()         #
 
-#img = Image.open("bild3.jpg")
-#img = img.resize((round(img.size[0] / 5), round(img.size[1] / 5)), resample=Image.BICUBIC)
+camera = PiCamera()
 
-#io.setmode(io.BOARD)  #pinnumber instead of gpio numbers
-#io.setup(7,io.OUT)  #pin 7 als Ausgang
-
-
-filename = "Untitled Folder\cardetector.h5"
+filename = "/home/pi/cardetector.h5"
 
 model = models.load_model(filename)
 
 ACCURACY_UP_TO = 0.98
-ACCURACY_DOWN_TO = 0.70
-
+#sizes of the clip to go through the images
 sizes = [
          400,350,300,
          250,200,150,
          100, 50,
             ]
 step_size = 50
-cars = []
 
-
-carDetectedAlready = True
-
-while True:
+def DoThatShit():
     #Getting Img
-    
-    img = Image.open("Untitled Folder\Bild3.jpg")
-    img = img.resize((round(img.size[0] * 5), round(img.size[1] * 5)), resample=Image.BICUBIC)
-    ##img = camera.captureJPEG(300, 200)
-    
-
-    #print(img)
+    camera.start_preview()
+    #wait to get a good image because of the light
+    sleep(3)
+    # get a picture and save it
+    camera.capture('/home/pi/actualPic.jpg')
+    camera.stop_preview()
+    #get the image from the folder
+    img = Image.open("/home/pi/actualPic.jpg")
+    #make the image smaller to get the programm faster
+    img = img.resize((round(img.size[0]/5), round(img.size[1]/5)), resample=Image.BICUBIC)
+    #plt.imshow(img)
+    #plt.show()
     #Reshaping
-    for size in sizes:
-        for x in range(0, img.size[0]- 100, step_size):
+    for size in sizes:      #go trough all the sizes
+        for x in range(0, img.size[0]- 100, step_size): #go trough the images
             for y in range(0, img.size[1] - size, step_size):
-                part = img.crop((x, y, x + size, y + size))
-                data =np.asarray(part.resize((32,32), resample=Image.BICUBIC))
-                data = data.astype(np.float32)/ 255.
-
-                pred = model.predict(data.reshape(-1, 32, 32, 3))
+                part = img.crop((x, y, x + size, y + size)) #get a clipping from the image
+                data =np.asarray(part.resize((32,32), resample=Image.BICUBIC))  #make it in a numpy array
+                data = data.astype(np.float32)/ 255.#make the numbers little
+                pred = model.predict(data.reshape(-1, 32, 32, 3)) #make a prediction
                 
-                if pred[0][0] > ACCURACY_UP_TO:
-                    cars.append((x,y, size))
-                    if carDetectedAlready: 
-                        print("Car detected")
-                        ##io.output(7,io.HIGH) #pin 7 ein
-                        ##time.sleep(100)
-                        ##io.output(7,io.LOW) #pin 7 wieder aus
-                        ##io.cleanup()
-                        carDetectedAlready = False
-                    
-                    else:
-                        if pred[0][0] < ACCURACY_DOWN_TO:
-                            carDetectedAlready = True
-                    
-                    
-                    
-                #plt.imshow(part)
-                #plt.show()
-                #else:
-                   # print("No Car detected")
-            
-    out = img.copy()
-    draw = ImageDraw.Draw(out)
-    #img.show(out)
-
-    cars_drawn = []
+                if pred[0][0] > ACCURACY_UP_TO:     #if there is a probability over 98%
+                    print("Car detected")
+                    io.setmode(io.BOARD)  #pinnumber instead of gpio numbers
+                    io.setup(31,io.OUT)  #pin 31 als Ausgang
+                    io.output(31,io.HIGH) #pin 31 ein
+                    time.sleep(30)
+                    io.output(31,io.LOW) #pin 31 wieder aus
+                    io.cleanup()
+                    return      #there was a car  and the door was opend then return
+while True:
+    DoThatShit()
